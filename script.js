a9e0cff42e86d3b95413707b6d46f10d644e7294
김예리 <dpfl0445@gmail.com>
2022-04-02 20:11:34 +0900

    create mouse circle

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ $(document).ready(() => {
         }
     }
 });
+
+const circle = document.querySelector(".circle");
+
+document.addEventListener("mousemove", (e) => {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    circle.style.left = mouseX + "px";
+    circle.style.top = mouseY + "px";
+});
