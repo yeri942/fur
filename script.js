da32b6d66f6f227af045dff1713a27eb6116f162
김예리 <dpfl0445@gmail.com>
2022-04-03 23:38:29 +0900

    create 마우스 위치와 fur위치가 겹칠시 콘솔에 메세지 찍어보기

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 $(document).ready(() => {
     for (let j = 0; j < 16; j++) {
-        for (let i = 0; i < 1200; i++) {
+        for (let i = 0; i < 200; i++) {
             $("#furArea").append(
-                `<div class="aFur${i}" style="transform:translateX(${
-                    0.04 * i
+                `<div class="aFur" style="transform:translateX(${
+                    0.25 * i
                 }vh) translateY(${3 * j}vh)"></div>`
             );
         }
     }
-});
+    const circle = document.querySelector(".circle");
+    const aFur = document.getElementsByClassName("aFur");
 
-const circle = document.querySelector(".circle");
-
-document.addEventListener("mousemove", (e) => {
-    const mouseX = e.clientX;
-    const mouseY = e.clientY;
-    circle.style.left = mouseX + "px";
-    circle.style.top = mouseY + "px";
+    document.addEventListener("mousemove", (e) => {
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
+        circle.style.left = mouseX + "px";
+        circle.style.top = mouseY + "px";
+        if (Math.round(aFur[0].getBoundingClientRect().top) == mouseY) {
+            console.log("헐!");
+        }
+    });
 });
