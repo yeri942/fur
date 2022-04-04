0c989d7c5edddad5cff62e30c446666204f9a305
김예리 <dpfl0445@gmail.com>
2022-04-04 23:52:01 +0900

    update 코드 정리

--- script.js
+++ script.js
@@ -8,15 +8,14 @@ $(document).ready(() => {
             );
         }
     }
-    const circle = document.querySelector(".circle");
-    const aFur = document.getElementsByClassName("aFur");
+    const circle = $(".circle");
+    const aFur = $(".aFur");
 
     document.addEventListener("mousemove", (e) => {
         const mouseX = e.clientX;
         const mouseY = e.clientY;
-        circle.style.left = mouseX + "px";
-        circle.style.top = mouseY + "px";
-        if (Math.round(aFur[0].getBoundingClientRect().top) == mouseY) {
+        circle.css("left", mouseX + "px").css("top", mouseY + "px");
+        if (Math.round(aFur.getBoundingClientRect().top) == mouseY) {
             console.log("헐!");
         }
     });
